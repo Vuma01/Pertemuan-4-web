daece27101f8c8ee18d3cb6f73a21b650f305120
CodeVuma <comvuma@gmail.com>
2025-10-30 17:03:02 +0900

    こんにちは

---
+++ script.js
@@ -0,0 +1,77 @@
+
+
+// kode standar 
+const name = "Hallo "; 
+// ini komentar
+
+
+//Data type - Number
+let int = 5;
+let float = 5.5;
+
+// Data type - String
+let kalimat = "こんにちは"; 
+
+
+// Data type - Boolean
+let booleanTrue = 2;
+let booleanFalse = 3;
+console.log(booleanTrue < booleanFalse); 
+console.log(booleanTrue > booleanFalse); 
+
+// variabel conts let
+ const apiUrl = "https://example.com";
+ let counter = 0
+
+  counter++;   // boleh diubah
+ console.log(apiUrl);    // "https://example.com
+
+
+//  Javascript Return
+
+function tambah(a, b) {
+ return a + b;  
+ }
+ let hasil = tambah(10, 4);
+ console.log(hasil);
+
+
+ // Operator - Arithmetic & Assignment
+
+//    let artA = 4;
+//    let artB = 3;
+//    artC = artA + artB;
+//    console.log(artC)
+
+//    Operator - Comparator
+
+ let artC = 4;
+   let artD = 2;
+   
+   if (artC > artD) {
+        alert( "artA lebih besar dari artC");
+    }
+
+//Statement – For and While
+    for (let i = 0; i < 5; i++) {
+        console.log("Perulangan ke-" + i);
+    }
+    let j = 0;
+    while (j < 5) {
+        console.log("Perulangan ke-" + j);
+        j++;
+    }
+
+
+    // Function
+    //User-Defined Function
+    function sapa(nama) {
+        return "Halo, " + nama + "!";
+    }
+    
+
+// Array
+    let buah = ["Apel", "Jeruk", "Mangga"];
+    console.log(buah[0]);
+
+
